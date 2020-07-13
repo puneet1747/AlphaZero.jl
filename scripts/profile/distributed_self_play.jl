b2d692014a0e6beb7d03a5b1b63484f8694c52fb
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2020-07-13 12:26:22 -0400

    proposed fix

--- scripts/profile/distributed_self_play.jl
+++ scripts/profile/distributed_self_play.jl
@@ -7,6 +7,11 @@ ENV["JULIA_CUDA_MEMORY_POOL"] = "split"
 using AlphaZero
 using ProgressMeter
 
+using Knet
+Knet.conv4_algo(w::KnetArray{T}, x::KnetArray{T}, y::KnetArray{T}; handle=Knet.cudnnhandle(), o...) where {T} = (0,0)
+Knet.conv4w_algo(w::KnetArray{T},x::KnetArray{T},dy::KnetArray{T},dw::KnetArray{T}; handle=Knet.cudnnhandle(), o...) where {T} = (0,0)
+Knet.conv4x_algo(w::KnetArray{T},x::KnetArray{T},dy::KnetArray{T},dx::KnetArray{T}; handle=Knet.cudnnhandle(), o...) where {T} = (0,0)
+
 include("../games.jl")
 GAME = get(ENV, "GAME", "connect-four")
 SelectedGame = GAME_MODULE[GAME]
